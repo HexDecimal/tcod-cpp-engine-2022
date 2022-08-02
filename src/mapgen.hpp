ced53cbb12a32e7411385f2feffbbf7cfb079c47
Kyle Benesch <4b796c65+github@gmail.com>
2022-08-01 19:58:34 -0700

    Add trolls.

--- src/mapgen.hpp
+++ src/mapgen.hpp
@@ -178,6 +178,18 @@ inline auto generate_level(World& world) -> Map& {
     monster.ai = std::make_unique<action::BasicAI>();
     world.schedule.push_back(monster_id);
   }
+  for (int repeats{0}; repeats < 4; ++repeats) {
+    auto& [monster_id, monster] = *new_actor(world);
+    monster.pos = pop_random(floor_tiles, world.rng);
+    monster.name = "troll";
+    monster.ch = 'T';
+    monster.fg = tcod::ColorRGB{0, 127, 0};
+    monster.stats.max_hp = monster.stats.hp = 16;
+    monster.stats.defense = 1;
+    monster.stats.attack = 4;
+    monster.ai = std::make_unique<action::BasicAI>();
+    world.schedule.push_back(monster_id);
+  }
 
   return map;
 }
